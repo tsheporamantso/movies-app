ba3221ec8b61f9b45655b8c7d99574b179870730
tsheporamantso <tshepo.ramantso@outlook.com>
2023-07-27 14:07:42 +0200

    Add useEffect hook

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -1,10 +1,14 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 
 const Counter = () => {
     const [counter, setCounter] = useState(0);
+
+    useEffect(() => {
+      setCounter(100);
+    },[]);
   return (
     <div style={{display:"grid", placeItems:"center"}}>
-        <button style={{fontSize:"30px"}} onClick={() => setCounter(counter - 1)}>-</button>
+        <button style={{fontSize:"30px"}} onClick={() => setCounter((prevCount) => prevCount - 1)}>-</button>
         <h1 style={{color:"yellow"}}>{counter}</h1>
         <button style={{fontSize:"30px"}} onClick={() => setCounter(counter + 1)}>+</button>
     </div>
