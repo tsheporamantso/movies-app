325e03e93c33c1e912d64a4897236fde9ecb7f5e
tsheporamantso <tshepo.ramantso@outlook.com>
2023-07-27 18:30:59 +0200

    Pass props and map through movies to render them dynamically

--- src/components/MoviesCards.jsx
+++ src/components/MoviesCards.jsx
@@ -1,23 +1,19 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import MovieCard from "./MovieCard";
 // import SearchIcon from './search.svg';
 
 const API_URL = "http://www.omdbapi.com?apikey=5b302ad3";
 
-const movie1 = {
-  Title: "Superman, Spiderman or Batman",
-  Year: "2011",
-  imdbID: "tt2084949",
-  Type: "movie",
-  Poster:
-    "https://m.media-amazon.com/images/M/MV5BMjQ4MzcxNDU3N15BMl5BanBnXkFtZTgwOTE1MzMxNzE@._V1_SX300.jpg",
-};
-
 const MoviesCards = () => {
+
+  const [movies, setMovies] = useState([]);
+
+  const [searchTerm, setSearchTerm] = useState("");
+
   const searchMovies = async (title) => {
     const response = await fetch(`${API_URL}&s=${title}`);
     const data = await response.json();
-    console.log(data.Search);
+    setMovies(data.Search);
   };
 
   useEffect(() => {
@@ -30,14 +26,29 @@ const MoviesCards = () => {
       <div className="search">
         <input
           placeholder="Search for movies"
-          value="Superman"
-          onChange={() => {}}
+          value={searchTerm}
+          onChange={(e) => setSearchTerm(e.target.value)}
         />
-        <img src="{SearchIcon}" alt="search" onClick={() => {}} />
+        <img src="{SearchIcon}" alt="search" onClick={() => searchMovies(searchTerm)} />
       </div>
-      <div className="container">
-        <MovieCard movie1={movie1} />
-      </div>
+
+
+      {
+        movies?.length > 0
+          ? (
+            <div className="container">
+              {movies.map((movie) => (
+                <MovieCard movie={movie} />
+              ))}
+            </div>
+          ) : (
+            <div className="empty">
+              <h2> No movies found</h2>
+            </div>
+          )
+      }
+
+
     </div>
   );
 };
