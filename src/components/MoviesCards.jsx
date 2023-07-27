e2f78d28488d768728f1d4dc9bf5acfe4a6a65e0
tsheporamantso <tshepo.ramantso@outlook.com>
2023-07-27 19:34:47 +0200

    Fix eslint error

--- src/components/MoviesCards.jsx
+++ src/components/MoviesCards.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import MovieCard from "./MovieCard";
-import SearchIcon from './search.svg';
+// import SearchIcon from './search.svg';
 
 const API_URL = "http://www.omdbapi.com?apikey=5b302ad3";
 
