ad06211e01f2b1b3fa59b454dd57778a51c529fe
tsheporamantso <tshepo.ramantso@outlook.com>
2023-07-27 07:21:16 +0200

    Add inline style on buttons to increase font size

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -4,9 +4,9 @@ const Counter = () => {
     const [counter, setCounter] = useState(0);
   return (
     <div style={{display:"grid", placeItems:"center"}}>
-        <button onClick={() => setCounter(counter - 1)}>-</button>
+        <button style={{fontSize:"30px"}} onClick={() => setCounter(counter - 1)}>-</button>
         <h1 style={{color:"yellow"}}>{counter}</h1>
-        <button onClick={() => setCounter(counter + 1)}>+</button>
+        <button style={{fontSize:"30px"}} onClick={() => setCounter(counter + 1)}>+</button>
     </div>
   )
 }
